f6eb79775bfc670401db6a0c4e8473cad48a4c2a
herman bjørnerud <herman.bjorn@gmail.com>
2019-11-18 16:45:25 +0100

    fikset mer på bakgrunnbilde osv

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -13,6 +13,32 @@ window.addEventListener("scroll", function() {
      parallax.style.backgroundPositionY=  "0";
     }
      });
+
+var scrollpos = window.scrollY;
+var chart = document.querySelector(".chart");
+var scrolltop = chart.scrollTop;
+
+function add_class_on_scroll() {
+    chart.classList.add("circle-chart__circle");
+}
+
+function remove_class_on_scroll() {
+    chart.classList.remove("circle-chart__circle");
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+ remove_class_on_scroll();
+    if(scrollpos > scrolltop + 160){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
+
 });
 
 
